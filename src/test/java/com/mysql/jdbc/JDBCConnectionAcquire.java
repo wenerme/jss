b14c5f9b93c27c28844904ab5621fd621a15c2c8
wener <wenermail@gmail.com>
2014-11-23 23:49:50 +0800

    实现离线的事务补偿

---
+++ src/test/java/com/mysql/jdbc/JDBCConnectionAcquire.java
@@ -0,0 +1,17 @@
+package com.mysql.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import org.junit.Test;
+
+public class JDBCConnectionAcquire
+{
+    @Test
+    public void test() throws SQLException, NoSuchFieldException, IllegalAccessException
+    {
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "root");
+        ConnectionImpl impl = (ConnectionImpl) c;
+        MysqlIO io = impl.getIO();
+    }
+}
